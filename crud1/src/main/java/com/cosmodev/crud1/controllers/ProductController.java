2d2d222ec17e72ac783e2943658dbb4aec7286a0
Arthur Filipe Cosmo <arthurcosmoservices@gmail.com>
2026-05-30 00:56:14 -0300

    Hello world no insominia

--- crud1/src/main/java/com/cosmodev/crud1/controllers/ProductController.java
+++ crud1/src/main/java/com/cosmodev/crud1/controllers/ProductController.java
@@ -1,9 +1,15 @@
 package com.cosmodev.crud1.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
 }
